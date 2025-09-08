8a919089da88c5d13344c20ef2cf394a07a5207b
XRedCubeX <57260282+XRedCubeX@users.noreply.github.com>
2025-09-08 21:14:04 +0200

    kaeru: Add support for Lenovo Tab M10 HD (2nd Gen) WiFi and LTE

---
+++ board/lenovo/board-amar_row_lte.c
@@ -0,0 +1,40 @@
+//
+// SPDX-FileCopyrightText: 2025 XRed_CubeX <57260282+XRedCubeX@users.noreply.github.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <board_ops.h>
+
+void board_early_init(void) {
+    printf("Entering early init for Lenovo Tab M10HD (2nd Gen) LTE\n");
+}
+
+void board_late_init(void) {
+    printf("Entering late init for Lenovo Tab M10HD (2nd Gen) LTE\n");
+
+    uint32_t addr = 0;
+
+    // Suppresses the bootloader unlock warning shown during boot on
+    // unlocked devices. In addition to the visual warning, it also
+    // introduces an unnecessary 5-second delay.
+    //
+    // This patch get rid of the delay and the warning by forcing the
+    // function that holds the logic to always return 0 and therefore
+    // not executing the code that shows the warning.
+    addr = SEARCH_PATTERN(LK_START, LK_END, 0xB508, 0x4B0E, 0x447B);
+    if (addr) {
+        printf("Found orange_state_warning at 0x%08X\n", addr);
+        FORCE_RETURN(addr, 0);
+    }
+
+    // Disables the warning shown during boot when the device is unlocked and
+    // the dm-verity state is corrupted. This behaves like the previous lock
+    // state warnings, visual only, with no real impact.
+    //
+    // Same approach: patch the function to always return 0.
+    addr = SEARCH_PATTERN(LK_START, LK_END, 0x2802, 0xD000, 0x4770);
+    if (addr) {
+        printf("Found dm_verity_corruption at 0x%08X\n", addr);
+        FORCE_RETURN(addr, 0);
+    }
+}
