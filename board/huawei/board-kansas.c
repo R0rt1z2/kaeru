73e41c224bcb67ed19e7b66962530978a32e40f7
R0rt1z2 <me@r0rt1z2.com>
2025-10-19 14:59:40 +0200

    kansas: Refactor current board setup
    Both the Huawei Y5 2019 and the Honor 8A are essentially the
    same device, so we can advertise support for both models.
    
    Additionally, fix the incorrect addresses for the FBLOCK and
    USRLOCK states, document all existing patches, and introduce
    a new patch to remove the unattractive FASTBOOT screen.
    
    Change-Id: Ic2f6f376fa4fdc17de9cc23ec032a61d61afd005

--- board/huawei/board-kansas.c
+++ board/huawei/board-kansas.c
@@ -9,24 +9,16 @@
 #define VOLUME_UP 17
 #define VOLUME_DOWN 1
 
-// A function that adds the specified number of new lines.
-// This is useful if the bottom of the screen is broken and you need to display logs.
-void print_spacer(int count) {
-    for (int i = 0; i < count; i++) {
-        video_printf("\n");
-    }
-}
-
 void board_early_init(void) {
-    printf("Entering early init for Huawei Y5 2019\n");
+    printf("Entering early init for Huawei Y5 2019 / Honor 8A\n");
 
     // Likely unnecessary, but done as a precaution.
     //
     // These writes set all known lock states—USRLOCK, FBLOCK, and Widevine—to
     // 'unlocked'. While the bootloader may overwrite these later, we patch the
     // corresponding getters in `board_late_init` to enforce the unlocked state.
-    WRITE32(0x480f5ec4, 1);  // USERLOCK state: UNLOCKED
-    WRITE32(0x480f5edc, 1);  // FBLOCK   state: UNLOCKED
+    WRITE32(0x481773A8, 1);  // USERLOCK state: UNLOCKED
+    WRITE32(0x481773A4, 1);  // FBLOCK   state: UNLOCKED
     WRITE32(0x4817739C, 1);  // WIDEVINE state: UNLOCKED
 
     // Huawei uses OEMINFO to verify whether the device was officially unlocked.
@@ -36,13 +28,25 @@ void board_early_init(void) {
     // These patches disable that behavior, preventing an automatic relock
     // when unofficial unlock methods are used.
     NOP(0x4802DFF4, 2);
+
+    // Cosmetic patch to suppress the default Android logo with lock state info
+    // shown during boot. This has no functional impact, purely visual.
+    //
+    // To restore the original behavior, simply remove this patch.
+    FORCE_RETURN(0x480200C4, 0);
 }
 
 void board_late_init(void) {
-    printf("Entering late init for Huawei Y5 2019\n");
+    printf("Entering late init for Huawei Y5 2019 / Honor 8A\n");
 
-    // show_boot_warning
-    PATCH_MEM(0x480566A8, 0x4770, 0xBF00);
+    // Suppresses the bootloader unlock warning shown during boot on
+    // unlocked devices. In addition to the visual warning, it also
+    // introduces an unnecessary 5-second delay.
+    // 
+    // This patch get rid of the delay and the warning by forcing the
+    // function that holds the logic to always return 0 and therefore
+    // not executing the code that shows the warning.
+    FORCE_RETURN(0x480566A8, 0);
 
     // Huawei registers a wrapper around all OEM commands, effectively blocking
     // their execution, even those explicitly registered by LK.
@@ -84,13 +88,6 @@ void board_late_init(void) {
         set_bootmode(BOOTMODE_FASTBOOT);
     }
 
-    // Show the current boot mode on screen when not performing a normal boot.
-    // This is standard behavior in many LK images, but not in this one by default.
-    //
-    // Displaying the boot mode can be helpful for developers, as it provides
-    // immediate feedback and can prevent debugging headaches.
-    show_bootmode(get_bootmode());
-
     // Remove the useless commands for unlocking/locking/relocking the bootloader.
     //
     // Unlock is done by editing seccfg, locking by flashing stock LK.
@@ -99,4 +96,13 @@ void board_late_init(void) {
     NOP(0x4802a2da, 2); // oem unlock
     NOP(0x4802a27a, 2); // flashing lock
     NOP(0x4802a264, 2); // flashing unlock
+
+    // Show the current boot mode on screen when not performing a normal boot.
+    // This is standard behavior in many LK images, but not in this one by default.
+    //
+    // Displaying the boot mode can be helpful for developers, as it provides
+    // immediate feedback and can prevent debugging headaches.
+    if (get_bootmode() != BOOTMODE_NORMAL) {
+        show_bootmode(get_bootmode());
+    }
 }
