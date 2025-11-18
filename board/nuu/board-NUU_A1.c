9c695db83b48f59a40b9868e0edc269d90048e37
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-11-18 10:45:40 +0100

    kaeru: Add support for NUU A1
    Support for MT6580 SoC is also added

---
+++ board/nuu/board-NUU_A1.c
@@ -0,0 +1,14 @@
+//
+// SPDX-FileCopyrightText: 2025 Shomy 🍂
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <board_ops.h>
+
+void board_early_init(void) {
+    printf("Entering early init for NUU A1\n");
+}
+
+void board_late_init(void) {
+    printf("Entering late init for NUU A1\n");
+}
