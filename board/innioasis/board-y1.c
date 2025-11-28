7b3a95277d83d50c85fbfd8271ad2b4354adb36e
Leah! <25106569+RealFX-Code@users.noreply.github.com>
2025-11-28 21:39:32 +0100

    kaeru: Add support for Innioasis Y1

---
+++ board/innioasis/board-y1.c
@@ -0,0 +1,14 @@
+//
+// SPDX-FileCopyrightText: 2025 Leah <25106569+RealFX-Code@users.noreply.github.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <board_ops.h>
+
+void board_early_init(void) {
+    printf("Entering early init for Innioasis Y1\n");
+}
+
+void board_late_init(void) {
+    printf("Entering late init for Innioasis Y1\n");
+}
