ede2e52028db14a342ac7f5f43c1ef176abaa671
R0rt1z2 <me@r0rt1z2.com>
2025-12-04 00:41:14 +0100

    kaeru: Add support for BQ Aquaris E10 3G
    Change-Id: If552f0b2dfa2c6ac68164a25b866dbb35c9f75f9

---
+++ board/bq/board-kaito.c
@@ -0,0 +1,14 @@
+//
+// SPDX-FileCopyrightText: 2025 R0rt1z2 <me@r0rt1z2.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <board_ops.h>
+
+void board_early_init(void) {
+    printf("Entering early init for BQ Aquaris E10 3G\n");
+}
+
+void board_late_init(void) {
+    printf("Entering late init for BQ Aquaris E10 3G\n");
+}
