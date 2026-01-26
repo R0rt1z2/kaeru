701144020fac6e2cacd2b5126e39c09ceef2d88c
qiratdahaf <122544035+qiratdahaf@users.noreply.github.com>
2026-01-26 15:59:58 +0500

    Add Support for Digit Next Ultra

---
+++ board/digit/board-next_ultra.c
@@ -0,0 +1,14 @@
+//
+// SPDX-FileCopyrightText: 2026 Your name <your.email@example.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <board_ops.h>
+
+void board_early_init(void) {
+    printf("Entering early init for Digit Next Ultra\n");
+}
+
+void board_late_init(void) {
+    printf("Entering late init for Digit Next Ultra\n");
+}
