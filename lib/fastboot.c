41b7c37adeb22dee3591720020a58e3f90d67613
Roger Ortiz <me@r0rt1z2.com>
2026-04-06 23:28:06 +0200

    lib/fastboot: Add support for combined response style
    Some Motorola LK bootloaders don't expose separate fastboot_okay(),
    fastboot_fail() and fastboot_info() functions. Instead, they use a
    combined fastboot_send_response(status, fmt, ...) for OKAY/FAIL and
    fastboot_send_info(status, fmt, ...) for INFO responses.
    
    Add FASTBOOT_STYLE_COMBINED to handle this, and keep the standard
    style as the default for existing defconfigs.

--- lib/fastboot.c
+++ lib/fastboot.c
@@ -1,30 +1,56 @@
 //
-// SPDX-FileCopyrightText: 2025 Roger Ortiz <me@r0rt1z2.com>
+// SPDX-FileCopyrightText: 2025-2026 Roger Ortiz <me@r0rt1z2.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
 
 #include <lib/fastboot.h>
 
-void fastboot_info(const char* reason) {
-    ((void (*)(const char* reason))(CONFIG_FASTBOOT_INFO_ADDRESS | 1))(reason);
+#if defined(CONFIG_FASTBOOT_STYLE_COMBINED)
+
+static void (*const _send_response)(const char* status, const char* fmt, ...) =
+        (void*)(CONFIG_FASTBOOT_SEND_RESPONSE_ADDRESS | 1);
+
+static void (*const _send_info)(const char* status, const char* fmt, ...) =
+        (void*)(CONFIG_FASTBOOT_SEND_INFO_ADDRESS | 1);
+
+void fastboot_okay(const char* reason) {
+    _send_response("OKAY", reason);
 }
 
 void fastboot_fail(const char* reason) {
-    ((void (*)(const char* reason))(CONFIG_FASTBOOT_FAIL_ADDRESS | 1))(reason);
+    _send_response("FAIL", reason);
 }
+
+void fastboot_info(const char* reason) {
+    _send_info("INFO", reason);
+}
+
+#elif defined(CONFIG_FASTBOOT_STYLE_STANDARD)
 
 void fastboot_okay(const char* reason) {
-    ((void (*)(const char* reason))(CONFIG_FASTBOOT_OKAY_ADDRESS | 1))(reason);
+    ((void (*)(const char*))(CONFIG_FASTBOOT_OKAY_ADDRESS | 1))(reason);
 }
 
-void fastboot_register(const char* prefix, void (*handle)(const char* arg, void* data, unsigned sz),
+void fastboot_fail(const char* reason) {
+    ((void (*)(const char*))(CONFIG_FASTBOOT_FAIL_ADDRESS | 1))(reason);
+}
+
+void fastboot_info(const char* reason) {
+    ((void (*)(const char*))(CONFIG_FASTBOOT_INFO_ADDRESS | 1))(reason);
+}
+
+#else
+#error "No fastboot response style selected."
+#endif
+
+void fastboot_register(const char* prefix,
+                       void (*handle)(const char* arg, void* data, unsigned sz),
                        unsigned char security_enabled) {
-    ((void (*)(const char* prefix, void (*handle)(const char* arg, void* data, unsigned sz),
-               unsigned char security_enabled))(CONFIG_FASTBOOT_REGISTER_ADDRESS | 1))(
+    ((void (*)(const char*, void (*)(const char*, void*, unsigned),
+               unsigned char))(CONFIG_FASTBOOT_REGISTER_ADDRESS | 1))(
             prefix, handle, security_enabled);
 }
 
 void fastboot_publish(const char* name, const char* value) {
-    ((void (*)(const char* name, const char* value))(CONFIG_FASTBOOT_PUBLISH_ADDRESS | 1))(name,
-                                                                                           value);
+    ((void (*)(const char*, const char*))(CONFIG_FASTBOOT_PUBLISH_ADDRESS | 1))(name, value);
 }
