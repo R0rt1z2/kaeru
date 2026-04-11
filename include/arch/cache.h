8b41480ec8d329fe85a5205aa7aadc793f0e9373
Roger Ortiz <me@r0rt1z2.com>
2026-04-11 17:46:21 +0200

    arch: Refactor cache maintenance APIs

--- include/arch/cache.h
+++ include/arch/cache.h
@@ -8,10 +8,9 @@
 #include <inttypes.h>
 #include <stddef.h>
 
-#define CACHE_LINE 32
-
 #define ICACHE 1
 #define DCACHE 2
 #define UCACHE (ICACHE | DCACHE)
 
+void arch_clean_cache_range(uintptr_t start, size_t len);
 void arch_clean_invalidate_cache_range(uintptr_t start, size_t len);
