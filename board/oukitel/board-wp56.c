be48f188842e0fa9e6815ddcf4a67baf0ac8528b
R0rt1z2 <roger@r0rt1z2.com>
2026-05-23 16:34:18 +0200

    kaeru: Add support for Oukitel WP56
    Change-Id: I0ee84dac576b992e4e1d98551fc0f50ae6ae45e9

---
+++ board/oukitel/board-wp56.c
@@ -0,0 +1,14 @@
+//
+// SPDX-FileCopyrightText: 2026 R0rt1z2 <me@r0rt1z2.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <board_ops.h>
+
+void board_early_init(void) {
+    printf("Entering early init for Oukitel WP56\n");
+}
+
+void board_late_init(void) {
+    printf("Entering late init for Oukitel WP56\n");
+}
