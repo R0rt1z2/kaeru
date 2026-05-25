0b61ebe265d39dfe43ad802ac376905a0bd52b87
R0rt1z2 <roger@r0rt1z2.com>
2026-05-25 22:56:03 +0200

    penangf: Handle `bootonce-bootloader` messages
    Change-Id: I5a9089165d7a023d772b3454a08e69528c9a29dc

--- board/motorola/board-penangf.c
+++ board/motorola/board-penangf.c
@@ -295,6 +295,12 @@ void parse_bootloader_messages(void) {
         memset(&misc_msg, 0, sizeof(misc_msg));
         partition_write("misc", 0, (uint8_t *)&misc_msg, sizeof(misc_msg));
     }
+    else if (strncmp(misc_msg.command, "bootonce-bootloader", 19) == 0) {
+        printf("Found bootonce-bootloader, forcing fastboot\n");
+        set_bootmode(BOOTMODE_FASTBOOT);
+        memset(&misc_msg, 0, sizeof(misc_msg));
+        partition_write("misc", 0, (uint8_t *)&misc_msg, sizeof(misc_msg));
+    }
 }
 
 static void handle_recovery_boot(void) {
