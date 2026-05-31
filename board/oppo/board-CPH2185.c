b939dac20d4cf8d9a478f56f0eabd66fc0c2aa20
fantom3031 <mpiven69@gmail.com>
2026-05-31 14:09:32 +0500

    kaeru: Add support for OPPO A15/A15s
    Change-Id: Ia3a31606a8261ebc655ecd93a03f437f1169b17c

---
+++ board/oppo/board-CPH2185.c
@@ -0,0 +1,66 @@
+//
+// SPDX-FileCopyrightText: 2026 fantom3031 <mpiven69@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <board_ops.h>
+
+void board_early_init(void) {
+    printf("Entering early init for oppo A15/A15s\n");
+
+    uint32_t addr = 0;
+
+    // BBK added a verification check to ensure the device was officially unlocked.
+    // If the check fails, the bootloader exits fastboot mode and reboots.
+    //
+    // This is unnecessary, seccfg-based unlocks are already valid, so we patch
+    // the check to always return true, ensuring fastboot remains accessible.
+    addr = SEARCH_PATTERN(LK_START, LK_END, 0xE92D, 0x4FF0, 0xF5AD, 0x5DAC);
+    if (addr) {
+        printf("Found fastboot_unlock_verify at 0x%08X\n", addr);
+        FORCE_RETURN(addr, 0);
+    }
+
+    // Forcing get_vfy_policy to return 0 skips certificate
+    // verification for all partitions and firmware images (boot,
+    // recovery, dtbo, SCP, etc.) so the device can boot with
+    // modified or unsigned images.
+    addr = SEARCH_PATTERN(LK_START, LK_END, 0xB508, 0xF7FF, 0xFF75, 0xF3C0);
+    if (addr) {
+        printf("Found get_vfy_policy at 0x%08X\n", addr);
+        FORCE_RETURN(addr, 0);
+    }
+
+    // Same idea but for download policy, forcing get_dl_policy to return
+    // 0 ensures no partition is marked as download-forbidden, so flashing
+    // via fastboot works for all partitions.
+    addr = SEARCH_PATTERN(LK_START, LK_END, 0xB508, 0xF7FF, 0xFF6F, 0xF000);
+    if (addr) {
+        printf("Found get_dl_policy at 0x%08X\n", addr);
+        FORCE_RETURN(addr, 0);
+    }
+
+    // On unlocked devices, LK shows an orange state warning during boot
+    // that also introduces an unnecessary 5 second delay. Forcing the
+    // function to return 0 skips both the warning and the delay.
+    addr = SEARCH_PATTERN(LK_START, LK_END, 0xB508, 0x4B0E, 0x447B, 0x681B, 0x681B, 0x2B02);
+    if (addr) {
+        printf("Found orange_state_warning at 0x%08X\n", addr);
+        FORCE_RETURN(addr, 0);
+    }
+
+    // Disables the warning shown during boot when the device is unlocked and
+    // the dm-verity state is corrupted. This behaves like the previous lock
+    // state warnings, visual only, with no real impact.
+    //
+    // Patch the function to always return 0 and thus not display the warning.
+    addr = SEARCH_PATTERN(LK_START, LK_END, 0x2802, 0xd000, 0x4770, 0xb538, 0x2432);
+    if (addr) {
+        printf("Found dm_verity_corruption at 0x%08X\n", addr);
+        FORCE_RETURN(addr, 0);
+    }
+}
+
+void board_late_init(void) {
+    printf("Entering late init for oppo A15/A15s\n");
+}
