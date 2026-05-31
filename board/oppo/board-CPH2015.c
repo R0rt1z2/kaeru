6391ae64bf2fa6288d1bf540deb7ec0534e7ad5e
fantom3031 <mpiven69@gmail.com>
2026-05-31 14:08:21 +0500

    kaeru: Add support for OPPO A31
    Change-Id: I0f1d88649d8ffcd95b5860e64f1e8ee4c98dddf3

---
+++ board/oppo/board-CPH2015.c
@@ -0,0 +1,21 @@
+//
+// SPDX-FileCopyrightText: 2026 fantom3031 <mpiven69@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <board_ops.h>
+
+#define VOLUME_UP 17
+
+void board_early_init(void) {
+    printf("Entering early init for oppo A31\n");
+}
+
+void board_late_init(void) {
+    printf("Entering late init for oppo A31\n");
+
+    if (mtk_detect_key(VOLUME_UP)) {
+        set_bootmode(BOOTMODE_RECOVERY);
+        show_bootmode(get_bootmode());
+    }
+}
