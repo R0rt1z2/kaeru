00516731964d6ba3c33d75c766876c23a6e8a17f
R0rt1z2 <roger@r0rt1z2.com>
2026-06-21 01:54:53 +0200

    arm: Save/restore SCTLR.A in unaligned helpers
    disable_unaligned() always set A back on, which is wrong if it wasn't set
    to begin with. Return the old bit from enable_unaligned() and restore it
    instead.
    
    Change-Id: Ic13f0c9d8e3ce3d3be0c08ec3ad5e757c00d70a2

--- include/arch/cache.h
+++ include/arch/cache.h
@@ -16,5 +16,5 @@ void arch_clean_cache_range(uintptr_t start, size_t len);
 void arch_clean_invalidate_cache_range(uintptr_t start, size_t len);
 void arch_sync_cache_range(uintptr_t start, uint32_t size);
 
-void enable_unaligned(void);
-void disable_unaligned(void);
+uint32_t enable_unaligned(void);
+void restore_unaligned(uint32_t prev);
