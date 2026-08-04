90a88f1843907819e357a4cb1e3fce52507b75a1
Ben Grisdale <bengris32@protonmail.ch>
2026-08-04 02:52:32 +0100

    amazon: Split common and device specific code for MT8516
    Split the device specific and common code in preperation to
    support multiple Amazon MT8516 devices. These will be nearly
    all Echos, using the same exploits and requiring the same
    quirks, so it makes sense to commonise most of that.
    
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

---
+++ board/amazon/include/mt8516-common.h
@@ -0,0 +1,68 @@
+//
+// SPDX-FileCopyrightText: 2026 Ben Grisdale <bengris32@protonmail.ch>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#pragma once
+
+#include <board_ops.h>
+
+#if defined(CONFIG_AMAZON_CUPCAKE)
+#include "mt8516-cupcake.h"
+#else
+#error "Invalid device selection"
+#endif
+
+// LK functions
+static inline void cmd_flash(const char *arg, void *data, unsigned sz) {
+    ((void (*)(const char *, void *, unsigned))(FB_CMD_FLASH_FUNC_ADDR|1))(arg, data, sz);
+}
+
+static inline void cmd_erase(const char *arg, void *data, unsigned sz) {
+    ((void (*)(const char *, void *, unsigned))(FB_CMD_ERASE_FUNC_ADDR|1))(arg, data, sz);
+}
+
+static inline void cmd_reboot(const char *arg, void *data, unsigned sz) {
+    ((void (*)(const char *, void *, unsigned))(FB_CMD_REBOOT_FUNC_ADDR|1))(arg, data, sz);
+}
+
+static inline void cmd_oem_reboot_recovery(const char *arg, void *data, unsigned sz) {
+    ((void (*)(const char *, void *, unsigned))(FB_CMD_REBOOT_RECOVERY_FUNC_ADDR|1))(arg, data, sz);
+}
+
+static inline const char* get_boot_part(void) {
+    return ((const char* (*)(void))(GET_BOOT_PART_FUNC_ADDR|1))();
+}
+
+static inline void cmdline_append(const char *arg) {
+    ((void (*)(const char *))(CMDLINE_APPEND_FUNC_ADDR|1))(arg);
+}
+
+static inline void mdelay(unsigned long msecs) {
+    ((void (*)(unsigned long))(MDELAY_FUNC_ADDR|1))(msecs);
+}
+
+static inline struct device_t* mt_part_get_device(void) {
+    return ((struct device_t* (*)(void))(CONFIG_MT_PART_GET_DEVICE_ADDRESS|1))();
+}
+
+static inline part_t* mt_get_part(const char* name) {
+    return ((part_t* (*)(const char*))(CONFIG_MT_PART_GET_PARTITION_ADDRESS|1))(name);
+}
+
+// Optional hooks that a device can implement if required.
+#ifdef HAVE_EARLY_INIT
+void device_early_init(void);
+#endif
+
+#ifdef HAVE_LATE_INIT
+void device_late_init(void);
+#endif
+
+#ifdef HAVE_FASTBOOT_INIT
+void device_fastboot_init(void);
+#endif
+
+#ifdef HAVE_FASTBOOT_CMD_REBOOT
+void device_fastboot_cmd_reboot(const char *arg, void *data, unsigned sz);
+#endif
