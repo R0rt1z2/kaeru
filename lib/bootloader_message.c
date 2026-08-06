baaeca8b2a01e51629eef349b1220e3b2edc88b2
Ben Grisdale <bengris32@protonmail.ch>
2026-08-06 03:05:35 +0100

    lib: Add bootloader_message library
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

---
+++ lib/bootloader_message.c
@@ -0,0 +1,101 @@
+//
+// SPDX-FileCopyrightText: 2026 Ben Grisdale <bengris32@protonmail.ch>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+#include <lib/bootloader_message.h>
+#include <lib/bootmode.h>
+#include <lib/debug.h>
+#include <lib/recovery.h>
+#include <lib/storage.h>
+#include <lib/string.h>
+
+static bool read_misc_partition(void *p, size_t size, uint64_t offset) {
+    const char *name = CONFIG_MISC_PARTITION_NAME;
+    const struct part_info *part = storage_part_find(name);
+    if (!part) {
+        printf("Failed to find %s partition\n", name);
+        return false;
+    }
+
+    return storage_part_read(part, p, offset, size) == size;
+}
+
+static bool write_misc_partition(void *p, size_t size, uint64_t offset) {
+    const char *name = CONFIG_MISC_PARTITION_NAME;
+    const struct part_info *part = storage_part_find(name);
+    if (!part) {
+        printf("Failed to find %s partition\n", name);
+        return false;
+    }
+
+    return storage_part_write(part, p, offset, size) == size;
+}
+
+bool read_bootloader_message(struct bootloader_message *boot) {
+    return read_misc_partition(boot, sizeof(*boot),
+                               BOOTLOADER_MESSAGE_OFFSET_IN_MISC);
+}
+
+bool write_bootloader_message(struct bootloader_message *boot) {
+    return write_misc_partition(boot, sizeof(*boot),
+                                BOOTLOADER_MESSAGE_OFFSET_IN_MISC);
+}
+
+bool write_bootloader_command(const char *cmd, bool overwrite) {
+    struct bootloader_message boot;
+
+    if (!read_bootloader_message(&boot))
+        return false;
+
+    if (!overwrite && boot.command[0] != '\0') {
+        printf("%s: Bootloader command pending.\n", __func__);
+        return false;
+    }
+
+    memset(boot.command, 0, sizeof(boot.command));
+    if (cmd)
+        strncpy(boot.command, cmd, sizeof(boot.command) - 1);
+
+    return write_bootloader_message(&boot);
+}
+
+bool clear_bootloader_command(bool overwrite) {
+    return write_bootloader_command(NULL, overwrite);
+}
+
+bool write_reboot_bootloader(bool overwrite) {
+    return write_bootloader_command("bootonce-bootloader", overwrite);
+}
+
+bool write_reboot_recovery(bool overwrite) {
+    return write_bootloader_command("boot-recovery", overwrite);
+}
+
+bool set_bootmode_from_message(struct bootloader_message *boot) {
+    bootmode_t target = misc_command_to_bootmode(boot->command);
+
+    if (target != BOOTMODE_NORMAL) {
+        set_bootmode(target);
+
+        // Clear the bootloader message if needed.
+        if (target == BOOTMODE_FASTBOOT)
+            return clear_bootloader_command(true);
+    }
+
+    return true;
+}
+
+bool read_and_set_bootmode_from_message(void) {
+    struct bootloader_message boot;
+    if (!read_bootloader_message(&boot))
+        return false;
+
+    return set_bootmode_from_message(&boot);
+}
+
+bool cmd_reboot_write_message(const char *arg) {
+    const char *msg = reboot_target_to_misc_command(arg);
+
+    return msg ? write_bootloader_command(msg, true) : false;
+}
