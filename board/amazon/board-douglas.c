99532f2baa021a5ef0d9cff07a4fbb2180fe074f
Ben Grisdale <bengris32@protonmail.ch>
2026-08-07 02:30:00 +0100

    douglas: Handle bootmodes entirely on our own
    Change-Id: I48d58be708a6cb6b03d6e13bab52d2f802a111a4
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

--- board/amazon/board-douglas.c
+++ board/amazon/board-douglas.c
@@ -5,11 +5,23 @@
 
 #include <board_ops.h>
 
-#define RTC_PDN1 0x802c
+#define RTC_PDN1                0x802c
+#define RTC_PDN1_FAST_BOOT      0x2000
+#define RTC_PDN1_RECOVERY_MASK	0x0030
+
 #define VOLUME_UP   0
 #define VOLUME_DOWN 5
 
+enum mode_reason {
+    MODE_REASON_NONE = 0,
+    MODE_REASON_MISC = 1,
+    MODE_REASON_RTC = 2,
+    MODE_REASON_KEY = 3,
+};
+
 static struct {
+    enum mode_reason reason;
+    uint32_t rtc_pdn1;
     bool unlocked_critical;
     bool bypass_remap;
 } gd;
@@ -24,6 +36,57 @@ static inline void cmd_erase(const char *arg, void *data, unsigned sz) {
 
 static inline void cmd_reboot(const char *arg, void *data, unsigned sz) {
     ((void (*)(const char *, void *, unsigned))(0x4BD40010|1))(arg, data, sz);
+}
+
+static inline void rtc_writeif_unlock(void) {
+    return ((void (*)(void))(0x4BD28514|1))();
+}
+
+static inline void rtc_write_trigger(void) {
+    return ((void (*)(void))(0x4BD284E8|1))();
+}
+
+static inline void pwrap_read(uint32_t reg, uint32_t *val) {
+    return ((void (*)(uint32_t, uint32_t *))(0x4BD26C04|1))(reg, val);
+}
+
+static inline void pwrap_write(uint32_t reg, uint32_t val) {
+    return ((void (*)(uint32_t, uint32_t))(0x4BD26C14|1))(reg, val);
+}
+
+static inline bool mtk_detect_pmic_just_rst(void) {
+    return ((bool (*)(void))(0x4BD25FC0|1))();
+}
+
+static const char *modereason2str(enum mode_reason reason) {
+    switch (reason) {
+        case MODE_REASON_NONE:
+            return "None";
+        case MODE_REASON_MISC:
+            return "BCB";
+        case MODE_REASON_RTC:
+            return "RTC";
+        case MODE_REASON_KEY:
+            return "Volume Keys";
+        default:
+            return NULL;
+    }
+}
+
+static inline bool read_rtc_mode(uint32_t mask) {
+    // This comes from the saved RTC_PDN1 we got
+    // from real_boot_mode_select().
+	return !!(gd.rtc_pdn1 & mask);
+}
+
+static void clear_rtc_mode(uint32_t clr_bits) {
+	uint32_t pdn1;
+
+	rtc_writeif_unlock();
+	pwrap_read(RTC_PDN1, &pdn1);
+	pdn1 = pdn1 & ~clr_bits;
+	pwrap_write(RTC_PDN1, pdn1);
+	rtc_write_trigger();
 }
 
 static bool advance_partition_name(const char** partition) {
@@ -129,6 +192,57 @@ usage:
     fastboot_fail("Usage: fastboot oem part-remap [0|1]");
 }
 
+static void real_boot_mode_select(void) {
+    // We use this opportunity to grab RTC_PDN1 for use later,
+    // as RTC driver init will clear out the recovery bits,
+    // which is not what we want when we have to detect
+    // RTC recovery mode.
+    pwrap_read(RTC_PDN1, &gd.rtc_pdn1);
+
+    // Set bootmode to BOOTMODE_NORMAL for good measure.
+    set_bootmode(BOOTMODE_NORMAL);
+}
+
+static void boot_mode_select(void) {
+    // Clear out the reset flag from the PMIC. We really don't care
+    // about the return, but not calling this function could mess
+    // things up.
+    mtk_detect_pmic_just_rst();
+
+    // Act on any boot command left in misc before anything else, so a
+    // key press can still override it below.
+    read_and_set_bootmode_from_message();
+    if (get_bootmode() != BOOTMODE_NORMAL)
+        gd.reason = MODE_REASON_MISC;
+
+    // Amazon removed the ability to enter fastboot / recovery mode with
+    // the volume keys, we restore that functionality here.
+    if (mtk_detect_key(VOLUME_DOWN)) {
+        set_bootmode(BOOTMODE_FASTBOOT);
+        gd.reason = MODE_REASON_KEY;
+    } else if (mtk_detect_key(VOLUME_UP)) {
+        set_bootmode(BOOTMODE_RECOVERY);
+        gd.reason = MODE_REASON_KEY;
+    }
+
+    // If our bootmode is STILL normal after all that, give a chance for
+    // RTC to select the boot mode, for compatibility with stock OS.
+    if (get_bootmode() == BOOTMODE_NORMAL) {
+        if (read_rtc_mode(RTC_PDN1_FAST_BOOT)) {
+            clear_rtc_mode(RTC_PDN1_FAST_BOOT);
+            set_bootmode(BOOTMODE_FASTBOOT);
+            gd.reason = MODE_REASON_RTC;
+        } else if (read_rtc_mode(RTC_PDN1_RECOVERY_MASK)) {
+            clear_rtc_mode(RTC_PDN1_RECOVERY_MASK);
+            set_bootmode(BOOTMODE_RECOVERY);
+            gd.reason = MODE_REASON_RTC;
+        }
+    }
+
+    // We would normally handle KPOC here too, but Amazon disable that
+    // as well ¯\_(ツ)_/¯
+}
+
 static void fastboot_init_hook(const char *) {
     // Register our custom command(s).
     fastboot_register("flash:", cmd_flash_wrapper, 1);
@@ -169,6 +283,9 @@ void board_early_init(void) {
     char *selinux_enforce = (char *)0x4BD662F4;
     selinux_enforce[2] = '\0';
 
+    // Override LK's default boot mode handling.
+    PATCH_CALL(0x4BD23A6E, &real_boot_mode_select, TARGET_THUMB);
+
     // Disable built in fastboot commands.
     NOP(0x4BD3F9CA, 2); // fastboot flash
     NOP(0x4BD3F9D8, 2); // fastboot erase
@@ -184,30 +301,13 @@ void board_early_init(void) {
 void board_late_init(void) {
     printf("Entering late init for Fire HD 8 (2017)\n");
 
-    // Act on any boot command left in misc before anything else, so a
-    // key press can still override it below.
-    if (get_bootmode() == BOOTMODE_NORMAL) {
-        read_and_set_bootmode_from_message();
+    boot_mode_select();
 
-        // Amazon removed the ability to enter fastboot / recovery mode with
-        // the volume keys, we restore that functionality here.
-        if (mtk_detect_key(VOLUME_DOWN)) {
-            set_bootmode(BOOTMODE_FASTBOOT);
-        } else if (mtk_detect_key(VOLUME_UP)) {
-            set_bootmode(BOOTMODE_RECOVERY);
-        }
-    }
-
-    if (get_bootmode() != BOOTMODE_NORMAL && get_bootmode() != BOOTMODE_FASTBOOT) {
-        // Show the current boot mode on screen when not performing a normal boot.
-        // This is standard behavior in many LK images, but not in this one by default.
-        //
-        // Displaying the boot mode can be helpful for developers, as it provides
-        // immediate feedback and can prevent debugging headaches.
-        show_bootmode(get_bootmode());
-    }
-
-    if (get_bootmode() == BOOTMODE_FASTBOOT) {
-        video_printf(" => HACKED FASTBOOT mode - xyz, k4y0z, R0rt1z2, bengris32\n");
+    bootmode_t mode = get_bootmode();
+    if (mode == BOOTMODE_FASTBOOT) {
+        video_printf(" => HACKED FASTBOOT mode (%s) - xyz, k4y0z, R0rt1z2, bengris32\n",
+                     modereason2str(gd.reason));
+    } else if (mode != BOOTMODE_NORMAL) {
+        video_printf(" => %s mode (%s)...\n", bootmode2str(mode), modereason2str(gd.reason));
     }
 }
