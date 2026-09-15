f807cb4341272632f2ed9d6d7b648a1f7f981c88
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-09-15 08:29:43 +0700

    Vol + and power force to bootloader
    Signed-off-by: KanagawaYamada <albert.wesley.dion@gmail.com>

--- board/tecno/board-lh8n.c
+++ board/tecno/board-lh8n.c
@@ -12,13 +12,23 @@ void board_late_init(void) {
     // Disable Orange State Warning
     FORCE_RETURN(0x48251c70, 0);
 
+    bootmode_t mode = get_bootmode();
+
+    // ---------------------------------------------------------
+    // Hardware Volume Key Boot Mode Overrides
+    // ---------------------------------------------------------
+    if (mode == BOOTMODE_RECOVERY) {
+        // Vol+ detected -> Force Bootloader (Fastboot)
+        set_bootmode(BOOTMODE_FASTBOOT);
+        video_printf("\n>>> VOL+ DETECTED: FORCING BOOTLOADER <<<\n\n");
+    }
+
+    // Refresh mode in case it was modified
+    mode = get_bootmode();
+
     // If booting to Normal System (Not Recovery/TWRP):
     if (mode != BOOTMODE_RECOVERY) {
         WRITE16(0x48251e6e, 0x2300);     // Spoof boot state to green
-
-        WRITE16(0x482616dc, 0xBF00);    // Spoof VBMeta to locked
-
+        WRITE16(0x482616dc, 0xBF00);     // Spoof VBMeta to locked
     }
-    // If booting to TWRP (mode == BOOTMODE_RECOVERY):
-    // We leave the boot state as orange and unlocked.
 }
