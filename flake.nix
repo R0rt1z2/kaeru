1717e65b0ea517babaec6bb41336cbfb7ff946bd
shomy <git@itssho.my>
2026-09-19 01:13:02 +0200

    kaeru: Update flake.nix
    Signed-off-by: shomy <git@itssho.my>

--- flake.nix
+++ flake.nix
@@ -1,46 +1,46 @@
 {
   description = "Kaeru build environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   inputs.frostix = {
     url = "github:shomykohai/frostix";
-    inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
     ...
-  } @ inputs: {
-    devShells.x86_64-linux.default = let
-      pkgs = import nixpkgs {system = "x86_64-linux";};
-      frostix = inputs.frostix.packages.${pkgs.system};
-    in
-      pkgs.mkShell {
-        nativeBuildInputs = [
-          pkgs.git
-          pkgs.python3
-          pkgs.gnumake
-          pkgs.android-tools
-          pkgs.python3Packages.capstone
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    frostix = inputs.frostix.legacyPackages.${system};
+    gccToolchain = frostix.gcc-toolchain.gcc-arm-linux-gnueabihf-12;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = [
+        pkgs.git
+        pkgs.python3
+        pkgs.gnumake
+        pkgs.android-tools
+        pkgs.python3Packages.capstone
 
-          frostix.lkpatcher
-          frostix.gcc-arm-linux-gnueabihf
-        ];
+        frostix.lkpatcher
+        gccToolchain
+      ];
 
-        shellHook = ''
-          export CROSS_COMPILE=arm-none-linux-gnueabihf-
-          export CC=${frostix.gcc-arm-linux-gnueabihf}/bin/''${CROSS_COMPILE}gcc
-          export AS=${frostix.gcc-arm-linux-gnueabihf}/bin/''${CROSS_COMPILE}as
-          export AR=${frostix.gcc-arm-linux-gnueabihf}/bin/''${CROSS_COMPILE}ar
-          export LD=${frostix.gcc-arm-linux-gnueabihf}/bin/''${CROSS_COMPILE}ld
-          export OBJCOPY=${frostix.gcc-arm-linux-gnueabihf}/bin/''${CROSS_COMPILE}objcopy
-          export OBJDUMP=${frostix.gcc-arm-linux-gnueabihf}/bin/''${CROSS_COMPILE}objdump
-          export NM=${frostix.gcc-arm-linux-gnueabihf}/bin/''${CROSS_COMPILE}nm
-          export RANLIB=${frostix.gcc-arm-linux-gnueabihf}/bin/''${CROSS_COMPILE}ranlib
+      shellHook = ''
+        export CROSS_COMPILE=arm-none-linux-gnueabihf-
+        export CC=${gccToolchain}/bin/''${CROSS_COMPILE}gcc
+        export AS=${gccToolchain}/bin/''${CROSS_COMPILE}as
+        export AR=${gccToolchain}/bin/''${CROSS_COMPILE}ar
+        export LD=${gccToolchain}/bin/''${CROSS_COMPILE}ld
+        export OBJCOPY=${gccToolchain}/bin/''${CROSS_COMPILE}objcopy
+        export OBJDUMP=${gccToolchain}/bin/''${CROSS_COMPILE}objdump
+        export NM=${gccToolchain}/bin/''${CROSS_COMPILE}nm
+        export RANLIB=${gccToolchain}/bin/''${CROSS_COMPILE}ranlib
 
-          export MAKEFLAGS="-e --no-print-directory"
-        '';
-      };
+        export MAKEFLAGS="-e --no-print-directory"
+      '';
+    };
   };
 }
